0fb5f43cd7d4572f478ce1bc533c1a0076926c65
Simon Lance <sl484@exeter.ac.uk>
2020-12-07 17:29:39 +0000

    Setting run_param values to a quicker sim for test sims on 'ike'

--- IVP/master/tot_s/run_param_file.py
+++ IVP/master/tot_s/run_param_file.py
@@ -7,15 +7,15 @@ import numpy as np
 Lx, Lz = 2, 1                       # Domain size
 Nx, Nz = 256, 128                    # Number of
 Pr = 1.                             # Prandtl number
-Ra = 1.8e8                          # Rayleigh number
-Ta = 1e7
-Np = 0.5                              # Number of density scale heights
+Ra = 1.5e5                          # Rayleigh number
+Ta = 1e5
+Np = 1                              # Number of density scale heights
 m = 1.5                             # Polytropic index
 Phi = np.pi/4
 theta = 1 - np.exp(-Np/m)           # Dimensionaless inverse T scale height
 
-initial_timestep = 1.5e-6           # Initial timestep
-max_dt = 1e-5                       # max dt
+initial_timestep = 1.5e-5           # Initial timestep
+max_dt = 1e-4                       # max dt
 
 snapshot_freq = 1.5e-3              # Frequency snapshot files are outputted
 analysis_freq = 1.5e-4              # Frequency analysis files are outputted
